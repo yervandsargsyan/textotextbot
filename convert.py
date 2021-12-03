64cf2544e030414b080e7074b918907aa6f57cbe
yervandsargsyan <84045761+yervandsargsyan@users.noreply.github.com>
2021-12-04 00:43:33 +0400

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,6 +1,5 @@
 from pylatexenc.latex2text import LatexNodes2Text
 
-
 def convert(tex):
-    txt = LatexNodes2Text().latex_to_text(tex)
-    return txt
+    text = LatexNodes2Text().latex_to_text(tex)
+    return text
